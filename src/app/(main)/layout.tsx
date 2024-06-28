6785713d7926ae84942cabf558dd88f980e111be
mehulrana159 <mehulrana159@gmail.com>
2024-06-28 15:31:00 +0530

    map view

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -10,9 +10,8 @@ export default function RootLayout({
     <>
       <Navbar />
 
-      <main className="container p-4 sm:p-6 flex-1">{children}</main>
+      <main>{children}</main>
 
-      <Footer />
     </>
   );
 }
--- src/modules/Map/Map.tsx
+++ src/modules/Map/Map.tsx
@@ -5,8 +5,12 @@ import BaseGoogleMap from "./components/GoogleMap";
 const Map = () => {
   return (
     <div>
-      <InputPopover />
-      <BaseGoogleMap />
+      <div className="absolute z-10 m-20">
+        <InputPopover />
+      </div>
+      <div className=" h-[calc(100vh_-88px)] w-[100%]">
+        <BaseGoogleMap />
+      </div>
     </div>
   );
 };

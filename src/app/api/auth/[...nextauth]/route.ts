29c70f27dd6876186e45fc8ae9b08411752f4c35
ss <himanshu@syncsignature.com>
2024-07-03 14:13:29 +0530

    code refactror and auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,14 +1,7 @@
 import NextAuth, { AuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import AWS from "aws-sdk";
 import connectToDatabase from "@/lib/mongodb";
 import User from "models/User";
-
-// AWS.config.update({
-//   accessKeyId: process.env.NEXT_AUTH_AWS_ACCESS_KEY,
-//   secretAccessKey: process.env.NEXT_AUTH_AWS_SECRET_KEY,
-//   region: process.env.NEXT_AUTH_AWS_REGION,
-// });
 
 const authOptions: AuthOptions = {
   providers: [
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,32 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
+import { getToken } from "next-auth/jwt";
 
-const nonAuthPath = ["/login", "/register", "/email-verify"];
-const protectedRoutes = ["/profile"];
+const nonAuthPath = ["/login", "/register"];
+const protectedRoutes = ["/map", "/dashboard", "profile", "/"];
 
-export async function middleware(req: NextRequest) {}
+export async function middleware(req: NextRequest) {
+  const url = req.nextUrl.clone();
+  const token = await getToken({ req, secret: process.env.NEXTAUTH_SECRET });
+
+  if (nonAuthPath.includes(url.pathname)) {
+    if (token) {
+      url.pathname = "/";
+      return NextResponse.redirect(url);
+    }
+    return NextResponse.next();
+  }
+
+  if (protectedRoutes.includes(url.pathname)) {
+    if (!token) {
+      url.pathname = "/login";
+      return NextResponse.redirect(url);
+    }
+  }
+
+  return NextResponse.next();
+}
 
 export const config = {
-  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
